33a48210838783c455070317a040437b5838a2cb
jaejin Lee <iadev20@gmail.com>
2021-03-09 23:48:25 +0900

    bug fix

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import json
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoAlertPresentException
 
 
 def get_second(text):
@@ -32,42 +35,36 @@ def listen_class():
     sleep(1)
     subjects = driver.find_elements(By.CLASS_NAME, 'sub_open')
     for i in range(len(subjects)):
-        sleep(1)
         subjects = driver.find_elements(By.CLASS_NAME, 'sub_open')
         subjects[i].click()
-        sleep(1)
         weeks = driver.find_elements(By.CLASS_NAME, 'wb-on')
         for j in range(len(weeks)):
-            sleep(1)
             weeks = driver.find_elements(By.CLASS_NAME, 'wb-on')
             weeks[j].find_element(By.CLASS_NAME, 'wb-week-on').click()
-            sleep(1)
             elements = driver.find_elements(By.XPATH, '//div/ul/li[1]/ol/li[5]/div/div')
             for k in range(len(elements)):
-                elements = driver.find_elements(By.XPATH, '//div/ul/li[1]/ol/li[5]/div/div')
-                e = elements[k]
-                times = re.split(" / ", e.find_element(By.XPATH, './/div[3]').text)
+                wait = WebDriverWait(driver, 10)
+                wait.until(EC.presence_of_element_located((By.XPATH, '//div/ul/li[1]/ol/li[5]/div/div')))
+                element = driver.find_elements(By.XPATH, '//div/ul/li[1]/ol/li[5]/div/div')[k]
+                times = re.split(" / ", element.find_element(By.XPATH, './/div[3]').text)
                 last = get_second(times[1]) - get_second(times[0])
-                print(e.find_element(By.XPATH, './/div[1]').text)
+                print(element.find_element(By.XPATH, './/div[1]').text)
                 print(last)
                 if last > 0:
-                    sleep(1)
-                    e.find_element(By.XPATH, './/span').click()
+                    driver.execute_script("arguments[0].click();", element.find_element(By.XPATH, './/span'))
                     try:
-                        sleep(1)
                         alert = driver.switch_to.alert
                         alert.dismiss()
                         print('학습인정기간이 지나 출석시간으로 인정되지 않습니다.')
-                    except:
+                    except NoAlertPresentException:
                         sleep(last + 10)
                         driver.find_element(By.ID, 'close_').click()
                         print('출석 완료')
                         try:
                             alert = driver.switch_to.alert
-                            alert.dismiss()
-                            print('alert occur')
-                        except:
-                            print('no alert')
+                            alert.accept()
+                        except NoAlertPresentException:
+                            pass
             driver.get(lms_url + '/ilos/st/course/submain_form.acl')
         driver.get(lms_url + '/ilos/main/main_form.acl')
 
